8b6afaa2cce70c80f118df80fafaa86991aef73e
Dan Slocombe <dan.slocombe@hotmail.com>
2022-09-03 00:42:48 +0100

    Good snapping

--- relatable-mobile/src/App.js
+++ relatable-mobile/src/App.js
@@ -67,13 +67,129 @@ function App() {
   return (
     <div className="App" style={{}}>
       <h1>Relatable</h1>
-      <MakeTest />
+      <Wosh />
     </div>
   );
 }
 
 function dan_lerp(x0, x, k) {
   return (x0 * (k-1) + x) / k;
+}
+
+class Wosh extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = { scrollPos: 0, scroller : null, items : [], touching:false };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 20);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick = () => {
+    //this.setState((state, _props) => ({
+      //scrollPos : 0,
+    //}));
+    if (!this.state.items || this.state.items.length == 0)
+    {
+      return;
+    }
+
+    if (this.state.touching)
+    {
+      return;
+    }
+
+      let centre = 338/2 + this.state.scrollPos;
+      let min_dist = 1000;
+      let min_x = 0;
+      let min_width = 0;
+      let min_index = 0;
+
+      const lerp_k = 7;
+
+      let current_x = 0;
+      let clamp_x_min = this.state.items[0].clientWidth + this.state.items[1].clientWidth / 2;
+      if (centre < clamp_x_min) {
+        this.state.scroller.scrollLeft = dan_lerp(this.state.scrollPos, clamp_x_min - 338/2, 10);
+        return;
+      }
+
+      let clamp_x_max = -this.state.items[this.state.items.length - 2].clientWidth / 2;
+      for (let i = 0; i < this.state.items.length - 1; i++)
+      {
+        console.log("pushing right");
+          clamp_x_max += this.state.items[i].clientWidth
+      }
+      if (centre > clamp_x_max) {
+        console.log("pushing left");
+        this.state.scroller.scrollLeft = dan_lerp(this.state.scrollPos, clamp_x_max - 338/2, 10);
+        return;
+      }
+
+      for (let i = 0; i < this.state.items.length; i++)
+      {
+        let child = this.state.items[i];
+
+        let target_x = (current_x + child.clientWidth / 2);
+        let dist = Math.abs(centre - target_x);
+        if (dist < min_dist) {
+          min_index = i;
+          min_dist = dist;
+          min_width = child.clientWidth;
+          min_x = target_x;
+        }
+        current_x += child.clientWidth;
+      }
+
+      if (min_dist < 50) {
+        //e.target.scrollLeft = min_x - 338/2;
+        console.log("snapping");
+        //this.setState((state) => {
+          //scrollPos: dan_lerp(state.scrollPos, min_x - 338/2, lerp_k)
+        //});
+        this.state.scroller.scrollLeft = dan_lerp(this.state.scrollPos, min_x - 338/2, lerp_k);
+      }
+
+  };
+
+  render = () => {
+    const style = {
+      display: "flex",
+      border: "1px solid",
+      width: "320px",
+      height: "568px",
+      scrollSnapType: "x mandatory",
+      overflowX: "auto",
+      whiteSpace: "nowrap",
+      position: "relative",
+    };
+
+    const onScroll = (e) => {
+      this.setState({
+        scrollPos: e.target.scrollLeft,
+        items : e.target.childNodes,
+        scroller : e.target,
+      });
+    }
+
+    return (<div style={style}  onScroll={onScroll} onTouchStart={() => this.setState({touching: true})} onTouchEnd={() => this.setState({touching: false})}>
+        <div style = {{minWidth:"200px"}}>
+        </div>
+        {
+          testClues.map((clue, id) => (
+              make_clue_container_static(id, clue)
+          ))
+        }
+        <div style = {{minWidth:"200px"}}>
+        </div>
+    </div>);
+  }
 }
 
 function MakeTest() {
@@ -102,7 +218,15 @@ function MakeTest() {
       let min_width = 0;
       let min_index = 0;
 
+      const lerp_k = 7;
+
       let current_x = 0;
+      let clamp_x_min = e.target.childNodes[0].clientWidth + e.target.childNodes[1].clientWidth / 2;
+      if (centre < clamp_x_min) {
+        e.target.scrollLeft = dan_lerp(e.target.scrollLeft, clamp_x_min - 338/2, 10);
+        return;
+      }
+
       for (let i = 0; i < e.target.childNodes.length; i++)
       {
         let child = e.target.childNodes[i];
@@ -120,7 +244,7 @@ function MakeTest() {
 
       if (min_dist < 50) {
         //e.target.scrollLeft = min_x - 338/2;
-        e.target.scrollLeft = dan_lerp(e.target.scrollLeft, min_x - 338/2, 6);
+        e.target.scrollLeft = dan_lerp(e.target.scrollLeft, min_x - 338/2, lerp_k);
       }
 
       console.log(min_index);
@@ -138,41 +262,6 @@ function MakeTest() {
     </div>);
 }
 
-function MakeMouseMoveTest() {
-    const { dragStart, dragStop, dragMove, dragging } = useDrag();
-    const handleDrag = ({ scrollContainer }) => (
-      ev
-    ) =>
-    {
-
-      console.log("hello");
-      return dragMove(ev, (posDiff) => {
-        if (scrollContainer.current) {
-          scrollContainer.current.scrollLeft += posDiff;
-        }
-      });
-    }
-
-    return (
-    <div>
-      <ScrollMenu 
-        onMouseDown={() => dragStart}
-        onMouseUp={() => dragStop}
-        onMouseMove={handleDrag}
-      >
-        <div style = {{width:"200px"}}>
-        </div>
-        {
-          testClues.map((clue, id) => (
-              make_clue_container_static(id, clue)
-          ))
-        }
-        <div style = {{width:"200px"}}>
-        </div>
-      </ScrollMenu>
-    </div>);
-}
-
 const testWordSets = [
   ['Easter', 'Carrot', 'Fluff'],
   ['Hills', 'Milk', 'Fringe'],
@@ -183,9 +272,9 @@ const testWordSets = [
 const testClues = [
   'Hutch',
   'Concentrate',
-  'Blahblahblah',
-  'Qwerty',
-  'Typewriter',
+  //'Blahblahblah',
+  //'Qwerty',
+  //'Typewriter',
 ];
 
 class Relatable extends Component {
